f72260ee3bd6bfdfa9dbbfec98b9dddf152f832c
Josh Barthelmess <joshbarthelmess3@gmail.com>
2021-04-28 16:03:30 -0400

    added basic get and create functionality for lists and listItems

--- src/controller.rs
+++ src/controller.rs
@@ -1,5 +1,5 @@
-use actix_web::{web, /*post,*/ get, /*delete, put,*/  HttpResponse};
-//use crate::entities::{List, ListItem};
+use actix_web::{web, post, get, /*delete, put,*/  HttpResponse};
+use crate::entities::{List, ListItem};
 //use serde::Deserialize;
 use deadpool_postgres::{Client, Pool};
 use crate::error::ListError;
@@ -10,4 +10,27 @@ pub async fn get_lists(db_pool: web::Data<Pool>, web::Path((owner,)): web::Path<
     let pg_client: Client = db_pool.get().await.map_err(ListError::PoolError)?;
     let all_lists = db::get_lists_by_owner(&pg_client, owner).await?;
     Ok(HttpResponse::Ok().json(all_lists))
+}
+
+#[post("/lists")]
+pub async fn create_list(db_pool: web::Data<Pool>, req_body: web::Json<List>) -> Result<HttpResponse, ListError> {
+    let pg_client: Client = db_pool.get().await.map_err(ListError::PoolError)?;
+    let new_list = db::create_new_list(&pg_client, req_body.into_inner()).await?;
+    Ok(HttpResponse::Created().json(new_list))
+}
+
+#[get("/lists/{list_id}")]
+pub async fn get_list_items(db_pool: web::Data<Pool>, web::Path((list_id,)): web::Path<(i32,)>) -> Result<HttpResponse, ListError> {
+    let pg_client: Client = db_pool.get().await.map_err(ListError::PoolError)?;
+    let list_items = db::get_list_items(&pg_client, list_id).await?;
+    Ok(HttpResponse::Ok().json(list_items))
+}
+
+#[post("/lists/{list_id}")]
+pub async fn create_list_item(db_pool: web::Data<Pool>, web::Path((list_id,)): web::Path<(i32,)>, list_item: web::Json<ListItem>) -> Result<HttpResponse, ListError> {
+    let pg_client: Client = db_pool.get().await.map_err(ListError::PoolError)?;
+    let mut new_item = list_item.into_inner();
+    new_item.list_id = list_id;
+    let new_item = db::create_list_item(&pg_client, new_item).await?;
+    Ok(HttpResponse::Ok().json(new_item))
 }
--- src/db.rs
+++ src/db.rs
@@ -1,4 +1,4 @@
-use crate::entities::List;
+use crate::entities::{List, ListItem};
 use crate::error::ListError;
 use deadpool_postgres::Client;
 use tokio_pg_mapper::FromTokioPostgresRow;
@@ -13,4 +13,43 @@ pub async fn get_lists_by_owner(client: &Client, owner: String) -> Result<Vec<Li
         .map(|row| List::from_row_ref(row).unwrap())
         .collect()
     )
+}
+
+pub async fn create_new_list(client: &Client, new_list: List) -> Result<List, ListError> {
+    let stmt = "INSERT INTO list (list_title, list_owner) values ($1, $2) RETURNING $table_fields;";
+    let stmt = stmt.replace("$table_fields", &List::sql_table_fields());
+    let stmt = client.prepare(&stmt).await.unwrap();
+    client
+        .query(&stmt, &[&new_list.list_title, &new_list.list_owner])
+        .await?
+        .iter()
+        .map(|row| List::from_row_ref(row).unwrap())
+        .collect::<Vec<List>>()
+        .pop()
+        .ok_or(ListError::NotFound)
+}
+
+pub async fn get_list_items(client: &Client, list_id: i32) -> Result<Vec<ListItem>, ListError> {
+    let stmt = "SELECT * FROM list_item WHERE list_id = $1;";
+    let stmt = client.prepare(&stmt).await.unwrap();
+    Ok(client
+        .query(&stmt, &[&list_id])
+        .await?
+        .iter()
+        .map(|row| ListItem::from_row_ref(row).unwrap())
+        .collect())
+}
+
+pub async fn create_list_item(client: &Client, new_item: ListItem) -> Result<ListItem, ListError> {
+    let stmt = "INSERT INTO list_item (list_item_description, list_item_status, list_id) values ($1, $2, $3) RETURNING $table_fields;";
+    let stmt = stmt.replace("$table_fields", &ListItem::sql_table_fields());
+    let stmt = client.prepare(&stmt).await.unwrap();
+    client
+        .query(&stmt, &[&new_item.list_item_description, &new_item.list_item_status, &new_item.list_id])
+        .await?
+        .iter()
+        .map(|row| ListItem::from_row_ref(row).unwrap())
+        .collect::<Vec<ListItem>>()
+        .pop()
+        .ok_or(ListError::NotFound)
 }
